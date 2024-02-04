f44779a9aec4d5e1e9d4cb723967f6685b1ba3b9
Mestima <mestima@nilored.ru>
2024-02-04 19:39:36 +0300

    updated main.go

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"WebhookRunner/enums"
 	"WebhookRunner/handlers"
 	"fmt"
 )
 
 func main() {
-	var cmd string
-	fmt.Scan(&cmd)
-
-	cmdHandler := handlers.CommandHandler{}
-	cmdHandler.SetType(enums.Windows)
-	cmdHandler.SetCommand(cmd)
-	out, err := cmdHandler.Execute()
-	fmt.Println(out, err)
+	handler := handlers.WebHandler{}
+	handler.SetPort(8081)
+	err := handler.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
