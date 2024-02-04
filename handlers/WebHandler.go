f082438328a7c355414fa0890638a2f3b8b03524
Mestima <mestima@nilored.ru>
2024-02-04 20:07:25 +0300

    moved flags handling to main.go, port is string now (was int)

--- handlers/WebHandler.go
+++ handlers/WebHandler.go
@@ -4,53 +4,69 @@ import (
 	"WebhookRunner/enums"
 	"WebhookRunner/utils"
 	"errors"
-	"flag"
 	"fmt"
 	"net/http"
 )
 
 type WebHandler struct {
 	commandHandler CommandHandler
-	port           int
-	isLogCommands  bool
-	isLogOutput    bool
+	port           string
+	logCommands    bool
+	logOutput      bool
+	isMinimal      bool
 }
 
-func (handler *WebHandler) SetPort(port int) {
+func (handler *WebHandler) SetPort(port string) {
 	handler.port = port
 }
 
-func (handler *WebHandler) GetPort() int {
+func (handler *WebHandler) GetPort() string {
 	return handler.port
+}
+
+func (handler *WebHandler) SetLogCommands(value bool) {
+	handler.logCommands = value
+}
+
+func (handler *WebHandler) GetLogCommands() bool {
+	return handler.logCommands
+}
+
+func (handler *WebHandler) SetLogOutput(value bool) {
+	handler.logOutput = value
+}
+
+func (handler *WebHandler) GetLogOutput() bool {
+	return handler.logOutput
+}
+
+func (handler *WebHandler) SetMinimal(value bool) {
+	handler.isMinimal = value
+}
+
+func (handler *WebHandler) GetMinimal() bool {
+	return handler.isMinimal
 }
 
 func (handler *WebHandler) ListenAndServe() error {
 	handler.init()
 
-	if handler.port == 0 {
+	if handler.port == "" {
 		return errors.New("port value is not set for WebHandler")
 	}
 
 	http.HandleFunc("/", handler.executeCode)
-	fmt.Println("Server is listening on port " + fmt.Sprint(handler.port))
-	return http.ListenAndServe(":"+fmt.Sprint(handler.port), nil)
+	fmt.Println("Server is listening on port " + handler.port)
+	return http.ListenAndServe(":"+handler.port, nil)
 }
 
 func (handler *WebHandler) init() {
 	handler.commandHandler = CommandHandler{}
 
-	isMinimal := flag.Bool("minimal", false, "use /bin/sh instead of /bin/bash to execute scripts on Linux systems") // TODO move to a standalone handler
-	isLogCommands := flag.Bool("log-commands", false, "log received commands")
-	isLogOutput := flag.Bool("log-output", false, "log executions output")
-	flag.Parse()
-
-	handler.isLogCommands = *isLogCommands
-	handler.isLogOutput = *isLogOutput
-
 	if utils.IsWindows() {
 		handler.commandHandler.SetType(enums.Windows)
 	} else {
-		if *isMinimal {
+		if handler.isMinimal {
 			handler.commandHandler.SetType(enums.LinuxMinimal)
 		} else {
 			handler.commandHandler.SetType(enums.Linux)
@@ -71,11 +87,11 @@ func (handler *WebHandler) executeCode(w http.ResponseWriter, r *http.Request) {
 			} else {
 				fmt.Fprint(w, out)
 
-				if handler.isLogCommands {
+				if handler.logCommands {
 					fmt.Println(cmd)
 				}
 
-				if handler.isLogOutput {
+				if handler.logOutput {
 					fmt.Println(out)
 				}
 			}
--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"WebhookRunner/handlers"
+	"flag"
 	"fmt"
 )
 
 func main() {
+	isMinimal := flag.Bool("minimal", false, "use /bin/sh instead of /bin/bash to execute scripts on Linux systems")
+	logCommands := flag.Bool("log-commands", false, "log received commands")
+	logOutput := flag.Bool("log-output", false, "log executions output")
+	port := flag.String("port", "8080", "server port")
+	flag.Parse()
+
 	handler := handlers.WebHandler{}
-	handler.SetPort(8081)
+
+	handler.SetMinimal(*isMinimal)
+	handler.SetLogCommands(*logCommands)
+	handler.SetLogOutput(*logOutput)
+	handler.SetPort(*port)
+
 	err := handler.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
